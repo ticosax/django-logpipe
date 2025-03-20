b50076b8718912da1c152a0dfd87cad40da91e46
Nicolas Delaby <nicolas@dryad.net>
2025-03-20 10:07:07 +0100

    Pause between pages

--- logpipe/consumer.py
+++ logpipe/consumer.py
@@ -5,9 +5,9 @@ import concurrent.futures
 import logging
 import time
 
+import pydantic_core
 from django.db import models, transaction
 from rest_framework import serializers
-import pydantic_core
 
 from . import settings
 from .abc import (
@@ -88,6 +88,9 @@ class Consumer(Iterator[tuple[Record, Serializer]]):
                         raise e
                 if stop_event.is_set():
                     break
+            if pause := settings.get("KINESIS_LONG_POLLING_WAIT_TIME", 0):
+                logger.debug("pause between pages %ds", pause)
+                time.sleep(pause)
 
     def _error_handler(self) -> Generator[tuple[Record, Serializer], None, None]:
         while True:
